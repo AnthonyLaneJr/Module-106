20aa27e069022e957c4ff471feca2b7d7f4ea8c8
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-05-14 00:06:04 -0700

    Competency Report
    A simple task manager that stores all tasks in an online server, made using JavaScript.

--- task manager/scripts/script.js
+++ task manager/scripts/script.js
@@ -3,6 +3,7 @@ const iconNonImportant = "iImportant fa-solid fa-bug-slash";
 var important = false;
 var inputPanel = true;
 const hide = "hidden";
+var counter = 0;
 
 function toggleImportance() {
   console.log(`clicked`);
@@ -20,10 +21,16 @@ function togglePanel() {
   if (inputPanel) {
     console.log(`hidden`);
     $(`#form`).addClass(hide);
+    $(`#hideBtn`).html(`Show`);
+    $(`#list`).addClass(`relative`);
+    $(`button`).addClass(`btn-relative`);
     inputPanel = false;
   } else {
     console.log(`revealing`);
     $(`#form`).removeClass(hide);
+    $(`#hideBtn`).html(`Hide`);
+    $(`#list`).removeClass(`relative`);
+    $(`button`).removeClass(`btn-relative`);
     inputPanel = true;
   }
 }
@@ -61,11 +68,35 @@ function saveTask(e) {
     frequency,
     state
   );
-  console.log(task);
-  displayTask(task);
+
+  clearInputs();
+
+  $.ajax({
+    type: `post`,
+    url: `https://fsdiapi.azurewebsites.net/api/tasks/`,
+    data: JSON.stringify(task),
+    contentType: `application/json`,
+    success: function (res) {
+      console.log(`Task Saved`, res);
+      displayTask(task);
+      counter += 1;
+      $(`#pendingLabel`).html(`You have ${counter} Tasks Tracked`);
+    },
+    error: function (errorDetails) {
+      console.error(`Save failed`, errorDetails);
+    },
+  });
+}
+
+function clearInputs() {
   $(`input`).val(``);
   $(`textarea`).val(``);
+  $(`select`).val(`0`);
+  $(`#colorSel`).val(`#000000`);
+  important = true;
+  toggleImportance();
 }
+
 function getStatusText(state) {
   switch (state) {
     case `1`:
@@ -127,11 +158,56 @@ function displayTask(task) {
   </div>`;
   $(`#tasks`).append(syntax);
 }
+
+function fetchTasks() {
+  $.ajax({
+    type: `get`,
+    url: `https://fsdiapi.azurewebsites.net/api/tasks`,
+    success: function (res) {
+      let data = JSON.parse(res); //this is coding form string into object - string are from server repsonse
+      console.log(data);
+      //for loop over data
+      for (let i = 0; i < data.length; i++) {
+        //get every element inside the array
+        let task = data[i];
+        if (task.name === `Anthony`) {
+          counter += 1;
+          displayTask(task);
+          //validate display only if object applies to me-i.e. task name is unique to my task consctructor
+        }
+        $(`#pendingLabel`).html(`You have ${counter} Tasks Tracked`);
+      }
+      //send the elemtn to the display
+    },
+    error: function (err) {
+      console.error(`Error retrieving data`, err);
+    },
+  });
+}
+//delete req
+//api/produts/clear/name
+function deleteTasks() {
+  $.ajax({
+    type: `delete`,
+    url: `https://fsdiapi.azurewebsites.net/api/tasks/clear/Anthony`,
+    success: function () {
+      location.reload();
+      console.log(`Tasks Succesfully Deleted`);
+      $(`#pendingLabel`).html(``);
+    },
+    error: function (err) {
+      console.error(`Unable to Delete Tasks`, err);
+    },
+  });
+}
 function init() {
   console.log(`task manager page`);
   $(`#saveBtn`).click(saveTask);
   //assign event and load data -init purpose
   $(`#iImportant`).click(toggleImportance);
+
+  //load data
+  fetchTasks();
 }
 
 window.onload = init;
